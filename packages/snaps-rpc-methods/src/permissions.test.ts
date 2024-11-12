d9ff46272e3d3da0d8a58ca132b9235eca3ce11f
Frederik Bolding <frederik.bolding@gmail.com>
2024-11-12 11:48:03 +0100

    Add basic tests

--- packages/snaps-rpc-methods/src/permissions.test.ts
+++ packages/snaps-rpc-methods/src/permissions.test.ts
@@ -91,6 +91,20 @@ describe('buildSnapEndowmentSpecifications', () => {
           ],
           "targetName": "endowment:page-settings",
         },
+        "endowment:protocol": {
+          "allowedCaveats": [
+            "chainIds",
+            "snapRpcMethods",
+            "maxRequestTime",
+          ],
+          "endowmentGetter": [Function],
+          "permissionType": "Endowment",
+          "subjectTypes": [
+            "snap",
+          ],
+          "targetName": "endowment:protocol",
+          "validator": [Function],
+        },
         "endowment:rpc": {
           "allowedCaveats": [
             "rpcOrigin",
